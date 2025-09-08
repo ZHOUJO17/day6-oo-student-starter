ea04d70509395bc4c83e25dc47cfe27151627f07
Jonny Zhou <jonny.zhou@oocl.com>
2025-09-08 11:55:03 +0800

    feat: a student can introduce himself

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,29 +1,14 @@
 package oo;
 
-public class Student {
-    private int id;
-    private String name;
-    private int age;
+public class Student extends Person{
+    private static final String STUDENT_LABEL = "I am a student.";
 
     public Student(int id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public String introduce(){
-        return String.format("My name is %s. I am %d years old. I am a student.",name,age);
-    }
-
-
-    @Override
-    public boolean equals(Object obj) {
-        Student person = (Student) obj;
-        return id == person.id;
+        super(id, name, age);
     }
 
     @Override
-    public int hashCode() {
-        return id;
+    public String introduce() {
+        return super.introduce() + " " + STUDENT_LABEL;
     }
 }
--- src/test/java/oo/step2/StudentTest.java
+++ src/test/java/oo/step2/StudentTest.java
@@ -5,6 +5,7 @@ import oo.Student;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class StudentTest {
 
@@ -14,7 +15,20 @@ public class StudentTest {
         // when introduce a student, then return a message with name and age
         assertEquals("My name is Tom. I am 18 years old. I am a student.",student.introduce());
     }
-    // when equals two same id student then return true
 
-    // when equals two different id student then return true
+    @Test
+    void should_get_true_when_equals_given_two_persons_with_same_id() {
+        Person student = new Person(0, "Tom", 18);
+        Person student1 = new Person(0,"Jonny",19);
+        // when equals two same id student then return true
+        assertEquals(student,student1);
+    }
+
+    @Test
+    void should_get_false_when_equals_given_two_persons_with_different_id() {
+        Person student = new Person(0, "Tom", 18);
+        Person student1 = new Person(1,"Jonny",19);
+        // when equals two different id student then return true
+        assertNotEquals(student,student1);
+    }
 }
