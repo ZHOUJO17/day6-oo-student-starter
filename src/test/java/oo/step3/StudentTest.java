e46dbeb9455ffd6877acb3994d43af678623dd43
Jonny Zhou <jonny.zhou@oocl.com>
2025-09-08 14:03:42 +0800

    feat: a student can be detected in class when he joined class

--- src/test/java/oo/step3/StudentTest.java
+++ src/test/java/oo/step3/StudentTest.java
@@ -10,13 +10,21 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StudentTest {
 
     @Test
-    void should_get_false_when_introduce_given_student() {
+    void should_get_false_when_isIn_given_not_join_class_student() {
         KClass kClass = new KClass(123);
         Student student = new Student(0, "Tom", 18);
         // when student not join klass and isIn klass then return false
         assertFalse(student.isIn(kClass));
     }
-    // when student not join klass and isIn klass then return false
+
+    @Test
+    void should_get_true_when_isIn_given_joined_class_student() {
+        KClass kClass = new KClass(123);
+        Student student = new Student(0, "Tom", 18);
+        student.join(kClass);
+        // when student not join klass and isIn klass then return false
+        assertTrue(student.isIn(kClass));
+    }
 
     // when student join klass and isIn klass then return false
 
